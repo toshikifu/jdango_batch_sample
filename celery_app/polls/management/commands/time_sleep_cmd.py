fc35bc277836b6979213dc1ba45720f495296ec7
Toshiki <toshiki.fujii@outlook.com>
2025-06-20 14:26:57 +0900

    get status from db cmd

--- celery_app/polls/management/commands/time_sleep_cmd.py
+++ celery_app/polls/management/commands/time_sleep_cmd.py
@@ -1,21 +1,36 @@
 import time
 from django.core.management.base import BaseCommand
+from django_celery_results.models import TaskResult
+from django.db.models import QuerySet
 
 from celery.result import AsyncResult
 
 from polls.tasks import time_sleep_func
 
+
 class Command(BaseCommand):
-    def handle(self, *args, **options):
+    def handle(self, *args, **options):  # type: ignore
         project_id: str = "0001"
-        task: AsyncResult = time_sleep_func.delay(project_id)
-        print("task status:", task.status)
+        task: AsyncResult = time_sleep_func.delay(project_id)  # type: ignore
+        print("task.status", task.status)  # type: ignore
 
-        task_id: str = task.id
-        print("task id:", task_id)
+        task_id: str = task.id  # type: ignore
+        print("task", task.id)  # type: ignore
 
-        task_1: AsyncResult = AsyncResult(task_id)
-        while not task.ready():
-            time.sleep(1)
-            print("task status:", task.status)
-            task_1 = AsyncResult(task_id)
+        time.sleep(5)
+
+        task_models: QuerySet[TaskResult] = TaskResult.objects.filter(task_id=task_id)
+        for task_model in task_models:
+            print("task_model.result", task_model.result)
+            print("task_model.date_created", task_model.date_created)
+            print("task_model.date_done", task_model.date_done)
+
+        time.sleep(10)
+
+        print("----" * 20)
+
+        task_models = TaskResult.objects.filter(task_id=task_id)
+        for task_model in task_models:
+            print("task_model.result", task_model.result)
+            print("task_model.date_created", task_model.date_created)
+            print("task_model.date_done", task_model.date_done)
